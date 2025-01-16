2c3bb737690167a9599f9e1d2bfd2814659ef871
Il Tuo Nome <tuoemail@example.com>
2025-01-16 08:44:00 +0100

    form inserimento

--- MotoriAjax/index.php
+++ MotoriAjax/index.php
@@ -6,22 +6,22 @@
         <body>
             <form>
             <label for="marca">Marca</label>
-            <input type="text" id="marca" name="marca" required>
+            <input type="text" id="marca" name="marca" required><br>
 
             <label for="modello">Modello</label>
-            <input type="text" id="modello" name="modello" required>
+            <input type="text" id="modello" name="modello" required><br>
 
             <label for="anno">Anno di Produzione</label>
-            <input type="number" id="anno" name="anno" min="1900" max="2025" required>
+            <input type="number" id="anno" name="anno" min="1900" max="2025" required><br>
 
             <label for="prezzo">Prezzo (€)</label>
-            <input type="number" id="prezzo" name="prezzo" step="0.01" required>
+            <input type="number" id="prezzo" name="prezzo" step="0.01" required><br>
 
             <label for="chilometraggio">Chilometraggio (km)</label>
-            <input type="number" id="chilometraggio" name="chilometraggio" required>
+            <input type="number" id="chilometraggio" name="chilometraggio" required><br>
 
             <label for="colore">Colore</label>
-            <input type="text" id="colore" name="colore" required>
+            <input type="text" id="colore" name="colore" required><br>
 
             <label for="stato">Stato</label>
             <select id="stato" name="stato" required>
